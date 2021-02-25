650410d52c46db7e53a3ee60020944267f60a3f9
edwinchiem732 <77710073+edwinchiem732@users.noreply.github.com>
2021-02-25 00:58:59 -0500

    Add files via upload

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,53 +1,219 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Client {
+//https://stackoverflow.com/questions/5667371/validate-ipv4-address-in-java
 
-	private static final String IP_REGEX = "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$" ;
+
+public class Client {
+	private BufferedReader in;
+	private BufferedReader reader;
+    private PrintWriter out;
+    private Socket socket;
+    private String serverAddress;
+    private int port;
+    
+    private static final String IP_REGEX = "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$" ;
 	private static final Pattern IP_PATTERN = Pattern.compile(IP_REGEX);
 
-	public static void main(String[] args) 
-	{
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Entrer votre adresse IP: ");
-		String adresseIP = scanner.nextLine();
-		while(!adresseIpValide(adresseIP)) {
-			System.out.println("Erreur: Votre adresse IP est invalide");
-			System.out.println("Entrer une  adresse IP  valide: ");
-			adresseIP = scanner.nextLine();
-		}
-		System.out.println("Entrer votre port d'écoute: ");
-		int port = scanner.nextInt();
-		while(port < 5000 ||  port > 5050  ) {
-			System.out.println("Erreur: Votre port d'ecoute dois etre entre 5000 et 5050 ");
-			System.out.println("Entrer un port d'écoute valide: ");
-			port = scanner.nextInt();
-		}
-		scanner.close();
-	}
-	 public static boolean adresseIpValide(String ip)
-   	{
-		 if (ip == null) 
-		 {
+ 	private static boolean verifyIp(String ip) {
+ 		if (ip == null) {
 	            return false;
 	     }
-        if (!IP_PATTERN.matcher(ip).matches())
-            return false;
-        String[] parts = ip.split("\\.");
-        try 
-        {
-            for (String segment: parts) 
-            {
-                if (Integer.parseInt(segment) > 255 || (segment.length() > 1 && segment.startsWith("0"))) 
-                {
-                    return false;
-                }
-            }
-        }catch(NumberFormatException e) 
-        {
-            return false;
+       if (!IP_PATTERN.matcher(ip).matches())
+           return false;
+       String[] segments = ip.split("\\.");
+       try {
+           for (String segment: segments) {
+               if (Integer.parseInt(segment) > 255 || (segment.length() > 1 && segment.startsWith("0"))) {
+                   return false;
+               }
+           }
+       }
+       catch(NumberFormatException e) {
+           return false;
+       }
+       return true;
+ 	}
+	
+	public boolean verifyPort(int port) {
+		return (port >= 5000 && port <= 5050);
+	}
+	
+	public int startPort() throws NumberFormatException, IOException{	
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Entrer votre port d'écoute: ");
+		int port = scanner.nextInt();
+        
+        while (!verifyPort(port)) {      	
+        	System.out.println("Erreur: Votre port d'ecoute dois etre entre 5000 et 5050 ");
+			System.out.println("Entrer un port d'écoute valide: ");
+			port = scanner.nextInt();
         }
-        return true;
+        return port;
+    }
+	
+	public String startIp() throws IOException{    
+  	    Scanner scanner = new Scanner(System.in);
+		System.out.println("Entrer votre adresse IP: ");
+		String adressIP = scanner.nextLine();
+  	    
+        while (!verifyIp(adressIP)) {      	
+        	System.out.println("Erreur: Votre adresse IP est invalide");
+			System.out.println("Entrer une  adresse IP  valide: ");
+			adressIP = scanner.nextLine();
+        }
+        return adressIP;
+    }
+    
+    private boolean verifyCommand(String command) {  		
+    	if (command.equals("ls") || command.equals("exit") || input1(command).equals("cd") || input1(command).equals("mkdir") || input1(command).equals("upload") 
+    			|| input1(command).equals("download")) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+    private String input1(String command){
+    	String input = "";
+        if (command.contains(" ")) {
+        	input = command.substring(0, command.indexOf(" "));
+        }
+        else {
+        	input = command;
+        }
+        return input;
+    }
+    
+    private String input2(String command) {
+    	String input = "";
+        if (command.contains(" ")) {
+        	input = command.substring(command.indexOf(" ") + 1, command.length());
+        }
+        return input;
+    }
+    
+    private boolean verifyFile(String fileName) {
+    	File file = new File(fileName);
+    	if (!(file.isFile())) {
+    		System.out.println("Le fichier n'existe pas.");
+    		return false;
+    	}
+    	return true;
+    }
+    
+    private void download(Socket sock, String fileName) throws IOException {  
+    	FileOutputStream output = new FileOutputStream(fileName);
+    	DataInputStream input = new DataInputStream(sock.getInputStream());
+    	int read = 0;
+		
+    	long fileSize = input.readLong();
+		byte[] buffer = new byte[5000];	
+		while(fileSize > 0 && (read = input.read(buffer)) > 0) {
+			output.write(buffer, 0, read);
+			fileSize -= read;
+		}
+		output.close();
+    }
+    
+    private void upload(Socket sock, File file) throws IOException { 
+    	FileInputStream input = new FileInputStream(file.toString());
+    	DataOutputStream output = new DataOutputStream(sock.getOutputStream());
+    	int read;
+		
+    	output.writeLong(file.length());
+		byte[] buffer = new byte[5000];
+		while ((read = input.read(buffer)) > 0) {
+			output.write(buffer, 0, read);
+		}
+		input.close();
+    }
+    
+    private void response(){
+        String response = "";
+        try {
+        	do {
+        		System.out.println(response);
+        		response = in.readLine();
+        	} 
+        	while (!response.equals("done"));
+        } 
+        catch (IOException ex) {
+        	response = "Erreur: " + ex;
+        }
+    }
+    
+    public void connectToServer() throws IOException {
+    	reader = new BufferedReader(new InputStreamReader(System.in));
+    	serverAddress = startIp();
+    	port = startPort();
+        socket = new Socket(serverAddress, port);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+    
+	public void run() throws IOException {	
+		connectToServer();	
+        System.out.println(in.readLine() + "\n");       
+        out.println(new File(".").toPath().toAbsolutePath());
+        out.println(serverAddress + " " + port);
+        String command = "";
+        
+        while (!command.equals("exit")) {
+	        System.out.println("\nTapez une commande:");
+	        command = reader.readLine();
+	        while (!verifyCommand(command)) {
+	            System.out.println(command + " n'existe pas. Choisissez une autre commande:");
+	        	command = reader.readLine();
+	        }	        
+	        if (input1(command).equals("upload")) {
+	        	if (verifyFile(input2(command))) {
+	    	        out.println(command);
+	    	        upload(socket, new File(input2(command)));
+	    	        response();
+	        	}
+	        }
+	        else if (input1(command).equals("download")) {
+    	        out.println(command);
+    	        String response = in.readLine();
+    	        if(response.equals("Telechargement")){    	        	
+    	        	download(socket, input2(command));
+    	        } 
+    	        else {    	        	
+    	        	System.out.println(response);
+    	        }
+	        	response();
+	        }
+	        else {
+		        out.println(command);
+	        	response();
+	        }
+    	}
+        if (command.equals("exit")) {
+            try {
+                socket.close();
+            } 
+            catch (IOException e) {
+                System.out.println("Couldn't close a socket, what's going on?");
+            }
+        	System.out.println("Vous avez ete deconnecte avec succes.");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.run();
     }
 
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,54 +1,106 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+
 
 public class Server {
 
 	private static final String IP_REGEX = "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$" ;
 	private static final Pattern IP_PATTERN = Pattern.compile(IP_REGEX);
-
-	public static void main(String[] args) 
-	{
+	private static ServerSocket listener;
+	
+	public static void main(String[] args) throws Exception {
+		int clientNumber = 0;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Entrer votre adresse IP: ");
-		String adresseIP = scanner.nextLine();
-		while(!adresseIpValide(adresseIP)) {
-			System.out.println("Erreur: Votre adresse IP est invalide");
-			System.out.println("Entrer une  adresse IP  valide: ");
-			adresseIP = scanner.nextLine();
+		
+		String serverAddress = "";
+		int serverPort =0;
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		while (true) {
+			serverAddress = evaluateAddress();
+			serverPort = evaluatePort();
+			try {
+				listener.bind(new InetSocketAddress(InetAddress.getByName(serverAddress), serverPort));
+				break;
+			} 
+			catch (IOException e) {
+				System.err.println("Erreur: Votre adresse IP est invalide");
+			}
 		}
-		System.out.println("Entrer votre port d'écoute: ");
-		int port = scanner.nextInt();
-		while(port < 5000 ||  port > 5050  ) {
-			System.out.println("Erreur: Votre port d'ecoute dois etre entre 5000 et 5050 ");
-			System.out.println("Entrer un port d'écoute valide: ");
-			port = scanner.nextInt();
-		}
+		
+		System.out.format("Le serveur roule sur %s:%d%n", serverAddress, serverPort);
 		scanner.close();
+		
+		try {
+			while(true) {
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+		}
+		finally {
+			listener.close();
+		}
 	}
-	 public static boolean adresseIpValide(String ip)
-   	{
-		 if (ip == null) 
-		 {
+	
+
+	public static Integer evaluatePort(){
+		Scanner scanner = new Scanner(System.in);
+	    String port = "";
+	    Boolean valeurValid = false;
+	    while (!valeurValid) {
+	    	try {
+	    		System.out.println("Entrer une valeur pour le port entre 5000 et 5050 :");
+	    		port = scanner.next();
+	    		if (Integer.parseInt(port) >= 5000 && Integer.parseInt(port) <= 5050) {
+	    			valeurValid = true;
+	    		}
+	    	} 
+	    	catch (Exception e) {
+	    		System.err.println("La valeur du port doit être un entier entre 5000 et 5050");
+	    	}
+	    }
+	    return Integer.parseInt(port);
+	}
+	
+
+	public static String evaluateAddress() {
+		Scanner scanner = new Scanner(System.in);
+		String deviceAddress = "";
+		Boolean adresseIpValide = false;
+		while (!adresseIpValide) {
+			try {
+				System.out.println("Entrer votre adresse IP: ");
+				deviceAddress=scanner.next();
+	    		adresseIpValide = adresseIpValide(deviceAddress);
+	    	} 
+			catch (Exception e) {
+				System.err.println("Erreur ");
+	    	}
+	    }
+		return deviceAddress;
+	}
+	
+	
+	public static boolean adresseIpValide(String ip) {
+		 if (ip == null) {
 	            return false;
 	     }
         if (!IP_PATTERN.matcher(ip).matches())
             return false;
         String[] parts = ip.split("\\.");
-        try 
-        {
-            for (String segment: parts) 
-            {
-                if (Integer.parseInt(segment) > 255 || (segment.length() > 1 && segment.startsWith("0"))) 
-                {
+        try {
+            for (String segment: parts) {
+                if (Integer.parseInt(segment) > 255 || (segment.length() > 1 && segment.startsWith("0"))) {
                     return false;
                 }
             }
-        }catch(NumberFormatException e) 
-        {
+        }
+        catch (NumberFormatException e) {
             return false;
         }
         return true;
     }
 }
-
-
